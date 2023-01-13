d6ec0271a2489860a9dc10a6b47202e26896e121
Maxim Tikachev <tika4ev55@gmail.com>
2023-01-13 19:25:55 +0300

    temp commit 53-58

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,33 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    private $posts = [
-        1 => [
-            'id' => 1,
-            'is_new' => false,
-            'has_comments' => true,
-            'title' => 'Post about Laravel Migrations',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam expedita vero blanditiis, fuga, sed ratione libero tempore ad beatae inventore, quos iste est similique accusamus ea molestias dolorem labore.'
-        ],
-        2 => [
-            'id' => 2,
-            'is_new' => true,
-            'title' => 'Post about Apache start on MacOS',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam expedita vero blanditiis, fuga, sed ratione libero tempore ad beatae inventore, quos iste est similique accusamus ea molestias dolorem labore.'
-        ],
-        3 => [
-            'id' => 3,
-            'is_new' => false,
-            'has_comments' => true,
-            'title' => 'Post about Laravel Routes',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam expedita vero blanditiis, fuga, sed ratione libero tempore ad beatae inventore, quos iste est similique accusamus ea molestias dolorem labore.'
-        ],
-    ];
-
     /**
      * Display a listing of the resource.
      *
@@ -36,7 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('post.index', ['posts' => $this->posts]);
+        $posts = BlogPost::all();
+        return view('post.index', ['posts' => $posts]);
     }
 
     /**
@@ -68,9 +47,10 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-
-        abort_if(!isset($this->posts[$id]), 404);
-        return view('post.show', ['id' => $id, 'post' => $this->posts[$id]]);
+        $post = BlogPost::all()->where('id', $id);
+        
+        abort_if(!isset($post), 404);
+        return view('post.show', ['id' => $id, 'post' => $post]);
     }
 
     /**

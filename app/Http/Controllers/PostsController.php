0da0c508dc1cd410f24d0c77f30ed8e36b3a8f0b
Maxim <tika4ev55@gmail.com>
2023-01-21 14:50:41 +0300

    Lesson 72-73 | Bootstrap installed

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,33 +9,17 @@ use Illuminate\Support\Facades\Redirect;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $posts = BlogPost::orderBy('created_at', 'asc')->get();
         return view('post.index', ['posts' => $posts]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('post.create', []);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StorePost $request)
     {
         $valid = $request->validated();
@@ -47,36 +31,17 @@ class PostsController extends Controller
         return Redirect::route('posts.show', ['post' => $result->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return view('post.show', ['id' => $id, 'post' => BlogPost::findOrFail($id)]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $post = BlogPost::findOrFail($id);
         return view('post.edit', ['post' => $post]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(StorePost $request, $id)
     {
         $post = BlogPost::findOrFail($id);
@@ -90,12 +55,6 @@ class PostsController extends Controller
         return Redirect::route('posts.show', $id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         BlogPost::findOrFail($id)->delete();
